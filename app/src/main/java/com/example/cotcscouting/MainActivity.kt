39d50deae2d09ca61f45f58df8c08aedc9f5c350
blueofficer <129885284+blueofficer@users.noreply.github.com>
2025-01-28 07:31:25 +0600

    DO NOT MERGE IM BEGGING
    ITs very bad please delete by 2/10/25

--- app/src/main/java/com/example/cotcscouting/MainActivity.kt
+++ app/src/main/java/com/example/cotcscouting/MainActivity.kt
@@ -1,38 +1,43 @@
-
-
-package com.example.cotcscouting
+package com.example.cotcscouting  // Make sure this matches your manifest package
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.AdapterView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupWithNavController
-import com.example.cotcscouting.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
-
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        val navView: BottomNavigationView = binding.navView
-        val navHostFragment = supportFragmentManager.findFragmentById(
-            R.id.nav_host_fragment_activity_main
-        ) as NavHostFragment
-        val navController = navHostFragment.navController
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration(
-            setOf(
-                R.id.navigation_blue_alliance, R.id.navigation_match_scouting, R.id.navigation_pit_scouting, R.id.navigation_alliance_scouting, R.id.navigation_hex_tech
-            )
+        setContentView(R.layout.spinner)  // This uses the R class
+
+        // Find the Spinner by its ID
+        val planetsSpinner: Spinner = findViewById(R.id.planets_spinner)
+
+        // Create an ArrayAdapter using the planets_array from resources
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.planets_array,  // Referencing array in strings.xml
+            android.R.layout.simple_spinner_item
         )
-        navView.setupWithNavController(navController)
-            LoginFragment().show(supportFragmentManager, "login_fragment")
+
+        // Set the layout for the dropdown view of the Spinner
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        // Set the adapter to the Spinner
+        planetsSpinner.adapter = adapter
+
+        // Set an item selected listener for the Spinner
+        planetsSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: android.view.View?, position: Int, id: Long) {
+                val selectedPlanet = parent.getItemAtPosition(position).toString()
+                println("Selected planet: $selectedPlanet")
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Handle case where no item is selected, if necessary
+            }
+        }
     }
-    //Comment to make this commitable
 }
