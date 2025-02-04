f68acfeb7f15a091043f21e92c87c0879870c4e7
blueofficer <129885284+blueofficer@users.noreply.github.com>
2025-02-04 07:38:49 +0600

    Finished the buttions
    Finshed the buttions
    
    -eldric

--- app/src/main/java/com/example/cotcscouting/MainActivity.kt
+++ app/src/main/java/com/example/cotcscouting/MainActivity.kt
@@ -24,6 +24,8 @@ class MainActivity : AppCompatActivity() {
             android.R.layout.simple_spinner_item
         )
 
+
+        // prosser points
         val ProcessorDec: Button = findViewById(R.id.ProcessorDec)
         val ProcessorInc: Button = findViewById(R.id.ProcessorInc)
         val textView: TextView = findViewById(R.id.ProcessorPoints)
@@ -33,7 +35,6 @@ class MainActivity : AppCompatActivity() {
             if (ProsserPoints > 0){
                 ProsserPoints--
                 textView.text = ProsserPoints.toString()
-
             }
         }
 
@@ -43,6 +44,71 @@ class MainActivity : AppCompatActivity() {
                 textView.text = ProsserPoints.toString()
             }
         }
+
+        // net points
+
+        val NetDec: Button = findViewById(R.id.NetDec)
+        val NetInc: Button = findViewById(R.id.NetInc)
+        val NetTextView: TextView = findViewById(R.id.NetPoints)
+        var NetPoints = 0
+
+        NetDec.setOnClickListener {
+            if (ProsserPoints > 0){
+                NetPoints--
+                NetTextView.text = NetPoints.toString()
+            }
+        }
+
+        NetInc.setOnClickListener {
+            if (true){
+                NetPoints++
+                NetTextView.text = NetPoints.toString()
+            }
+        }
+
+        //l1 points
+        val L1Dec: Button = findViewById(R.id.L1CoralDec)
+        val L1Inc: Button = findViewById(R.id.L1CoralInc)
+        val L1TextView: TextView = findViewById(R.id.L1Points)
+        var L1Points = 0
+        //when the time come for diferenting between teleop you can add a nother var
+
+        L1Dec.setOnClickListener {
+            if (L1Points > 0){
+                L1Points--
+                L1TextView.text = L1Points.toString()
+            }
+        }
+
+        L1Inc.setOnClickListener {
+            if (true){
+                L1Points++
+                L1TextView.text = L1Points.toString()
+            }
+        }
+
+        //BargePoints
+        val BargeDec: Button = findViewById(R.id.BargeDec)
+        val BargeInc: Button = findViewById(R.id.BargeInc)
+        val BargeTextView: TextView = findViewById(R.id.BargePoints)
+        var BargePoints = 0
+
+        BargeDec.setOnClickListener {
+            if (BargePoints > 0){
+                BargePoints--
+                BargeTextView.text = BargePoints.toString()
+            }
+        }
+
+        BargeInc.setOnClickListener {
+            if (true){
+                BargePoints++
+                BargeTextView.text = BargePoints.toString()
+            }
+        }
+
+
+
 
         // Set the layout for the dropdown view of the Spinner
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
