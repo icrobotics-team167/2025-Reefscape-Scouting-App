6390270d72be4093073f6ca2115812775e9f81c3
blueofficer <129885284+blueofficer@users.noreply.github.com>
2025-04-06 23:33:07 +0600

    More funny

--- app/src/main/java/com/example/scoutingappv3/Dependences/Config.java
+++ app/src/main/java/com/example/scoutingappv3/Dependences/Config.java
@@ -13,7 +13,7 @@ public class Config {
     public static int BotTracked = 0;
     //Team Number
     public static int TeamNumber = 0;
-    public static String[] TheFunnyMessages = {"Hello Dungeon!","Doint Scout And drive Tada!!!","10 People?!?!?!?!!","man I lOvE StringInconsitent var_Names","Hello SAR-I mean EMMA!!!!!", "Luzz iuzz Jaynuzz", "Why hello daniel >:)","Kazu How Could You Betray Tada  !!! " ,"Chicken Jockey, Farhan watch out!", "Hi Eldric! -Calder", "Are you playing Minecraft right now?...", "Hello EMMM-I mean SARA!!!!!","Thank You Elly For All Your Hard Work!!!!  :D","i hear You like sliders :D","Hello Mom-A-Tron"};
+    public static String[] TheFunnyMessages = {"Hello Dungeon!","Doint Scout And drive Tada!!!","26 People?!?!?!?!!","man I lOvE StringInconsitent var_Names","Hello SAR-I mean EMMA!!!!!", "Luzz iuzz Jaynuzz", "Why hello daniel >:)","Kazu How Could You Betray Tada  !!! " ,"Chicken Jockey, Farhan watch out!", "Hi Eldric! -Calder", "Are you playing Minecraft right now?...", "Hello EMMM-I mean SARA!!!!!","Thank You Elly For All Your Hard Work!!!!  :D","i hear You like sliders :D","Hello Mom-A-Tron"};
     public static String[] Names = {"Eldric","Tada","jinann","calder","emma","jaynou","daniel","kazu","farhan","jerry","evan","sara","elly","madison","momatron"};
     public static String UnknowUser = "Thank you for scouting! Give it a secend to load";
     public static Context AppContext;
